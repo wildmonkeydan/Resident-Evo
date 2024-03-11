db06febe362ca9ecc8fc15856eba99e958076a7e
wildmonkeydan <danturner580@gmail.com>
2024-03-11 17:44:59 +0000

    Zombie Blood Spatters

---
+++ Assets/Scripts/Zombie.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Zombie : MonoBehaviour
+{
+    public float walkRadius = 50f;
+    public GameObject blood;
+
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        FindNewPosition();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = agent.remainingDistance; 
+        if (dist != Mathf.Infinity && agent.pathStatus == NavMeshPathStatus.PathComplete && agent.remainingDistance == 0)
+        {
+            FindNewPosition();
+        }
+    }
+
+    void FindNewPosition()
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * walkRadius;
+
+        randomDirection += transform.position;
+        NavMeshHit hit;
+        NavMesh.SamplePosition(randomDirection, out hit, walkRadius, 1);
+        Vector3 finalPosition = hit.position;
+
+        agent.SetDestination(finalPosition);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            Instantiate(blood, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
